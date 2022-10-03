adb466f7f4022aab19a5a7f38704484e46bbceca
Chaudoreille <a.chaudoreille@gmail.com>
2022-10-03 12:18:18 +0200

    add tests for vertex
    - delete __iadd__ and __isub__ because it makes no sense for vertex

--- algo/graphs/tests/test-vertex.py
+++ algo/graphs/tests/test-vertex.py
@@ -4,11 +4,46 @@ from algo.graphs.vertex import Vertex
 
 class TestVertex(TestCase):
     def test_str(self):
-        v = Vertex("A", {"wieght": 1}, ["B"])
+        a = Vertex("A", [Vertex("B")])
         EXPECTED_RESULT = "A"
 
-        self.asssertEqual(str(v), EXPECTED_RESULT)
+        self.assertEqual(str(a), EXPECTED_RESULT)
 
+    def test_get_item(self):
+        b = Vertex("B")
+        a = Vertex("A", [b])
+
+        self.assertEqual(b, a["B"])
+
+    def test_set_item(self):
+        a = Vertex("A")
+        b = Vertex("B")
+
+        a["B"] = b
+        b["A"] = a
+
+        self.assertEqual(b, a["B"])
+        self.assertEqual(a, b["A"])
+    
+    def test_append(self):
+        a = Vertex("A")
+        b = Vertex("B")
+        c = Vertex("C")
+
+        a.append(b)
+        a.append(c)
+
+        self.assertEqual(b, a["B"])
+        self.assertEqual(c, a["C"])
+
+    def test_delitem(self):
+        a = Vertex("A", [Vertex("B")])
+        b = a["B"]
+
+        del a["B"]
+
+        self.assertRaises(KeyError, a.__getitem__, "B")
+        self.assertEqual(b.name, "B")
 
 if __name__ == "__main__":
     main()
--- algo/graphs/vertex.py
+++ algo/graphs/vertex.py
@@ -1,9 +1,10 @@
+from typing import Iterable
 import warnings
 
 
 class Vertex:
     @classmethod
-    def _all(container):
+    def _all(cls, container):
         def isVertex(object):
             if type(object) is not Vertex:
                 raise TypeError(f"{type(object)} is not a Vertex")
@@ -12,10 +13,9 @@ class Vertex:
         if type(container) is dict:
             container = container.values
 
-        map(isVertex , container)
-        return True
+        return all(map(isVertex , container))
 
-    def __init__(self, name, attributes=None, edges=None):
+    def __init__(self, name, edges=None, attributes=None):
         self.name = name
         self.attributes = {}
         self.edges = {}
@@ -32,6 +32,9 @@ class Vertex:
     def __str__(self):
         return str(self.name)
 
+    def __len__(self):
+        return len(self.edges)
+
     def __getitem__(self, key):
         return self.edges[key]
 
@@ -39,14 +42,6 @@ class Vertex:
         if type(vertex) is not Vertex:
             raise TypeError(f"{type(vertex)} is not a Vertex")
         self.edges[key] = vertex
-
-    def __iadd__(self, object):
-        if type(object) is Vertex:
-            self.edges += [object]
-    
-    def __isub__(self, object):
-        if type(object) is Vertex:
-            del self.edges[object.name]
 
     def __delitem__(self, key):
         del self.edges[key]
@@ -58,6 +53,3 @@ class Vertex:
             raise ValueError(f"{vertex} is already and edge of {self}")
 
         self.edges[vertex.name] = vertex
-
-    def __init__(self):
-        pass
